7a81a9dfb2f51e0494cb96eab0f2ccb0e3b22bb7
viniz0x <armazenamentovini@gmail.com>
2024-09-02 21:11:49 -0300

    desafio_completo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using Microsoft.Win32.SafeHandles;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -15,8 +17,9 @@ namespace DesafioFundamentos.Models
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            //************************************************************************************************
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
         }
 
         public void RemoverVeiculo()
@@ -24,22 +27,33 @@ namespace DesafioFundamentos.Models
             Console.WriteLine("Digite a placa do veículo para remover:");
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            //************************************************************************************************
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                //************************************************************************************************
+                string entrada = Console.ReadLine();
+                int horas;
+
+                //Validação se o número é valido ou não
+                bool conversaoBemSucedida = int.TryParse(entrada, out horas);
+                if (conversaoBemSucedida) {
+                    Console.WriteLine($"Você digitou: {horas} horas.");
+                } else {
+                    Console.WriteLine("Valor inválido, deve conter apenas números inteiros.");
+                    return;
+                }
+
+                decimal valorTotal = (precoInicial + precoPorHora * horas); 
 
                 // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                //************************************************************************************************
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -56,7 +70,10 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                //************************************************************************************************
+                foreach (string str in veiculos) {
+                    Console.WriteLine(str);
+                }
             }
             else
             {
